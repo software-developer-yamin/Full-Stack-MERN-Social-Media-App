7ca0ab6f7936053f78a5b60ad08bad61b6239bf7
DeveloperYamin <developer.yamin@gmail.com>
2022-03-25 19:10:09 +0600

    install all package and used

---
+++ server/index.js
@@ -0,0 +1,9 @@
+import bodyParser from "body-parser";
+import cors from "cors";
+import express from "express";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: "true" }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: "true" }));
+app.use(cors());
