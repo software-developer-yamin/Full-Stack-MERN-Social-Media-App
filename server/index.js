a708450a95c5fc5bdfdacd6042691325574f13c3
DeveloperYamin <developer.yamin@gmail.com>
2022-03-25 19:50:44 +0600

    test first routers

--- server/index.js
+++ server/index.js
@@ -3,11 +3,17 @@ import cors from "cors";
 import express from "express";
 import mongoose from "mongoose";
 
+// import routers
+import PostRoutes from "./routes/posts.js";
+
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: "true" }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: "true" }));
 app.use(cors());
+
+// use all routers
+app.use("/posts", PostRoutes);
 
 const CONNECTION_URL = "mongodb+srv://bdyamin:01880279@cluster0.ecjio.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
