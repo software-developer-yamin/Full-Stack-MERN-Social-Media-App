6d0e3c0068d6fe916337ea6e821effab7d6e31b7
DeveloperYamin <developer.yamin@gmail.com>
2022-03-25 19:38:39 +0600

    connecting mongodb and running server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 import bodyParser from "body-parser";
 import cors from "cors";
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: "true" }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: "true" }));
 app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://bdyamin:01880279@cluster0.ecjio.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+     .catch(err => console.log(err.message));
